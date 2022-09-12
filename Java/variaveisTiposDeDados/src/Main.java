faba630ffdf1edc269df7df0e40a927c2e3a740b
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-09-12 11:17:57 -0300

    Practice

---
+++ Java/variaveisTiposDeDados/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+        float two;
+        final int agora;
+        two = 20.0f;
+        System.out.println(two);
+
+        byte b1 = 20;
+        short ab = 20;
+        int a = 2;
+        float gg = 10f;
+        double agora2 = 2d;
+        char c1 = 'C';
+        String std = "Mame";
+        boolean agora3 = true;
+
+        System.out.println(b1);
+        System.out.println(ab);
+        System.out.println(a);
+        System.out.println(gg);
+        System.out.println(agora2);
+        System.out.println(c1);
+        System.out.println(std);
+    }
+
+
+}
