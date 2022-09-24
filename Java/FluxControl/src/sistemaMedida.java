1d7234268113fb7c0c3bcb8b97adb4686660d78d
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-09-24 12:44:48 -0300

    add sistemaMedida

---
+++ Java/FluxControl/src/sistemaMedida.java
@@ -0,0 +1,23 @@
+public class sistemaMedida {
+    public static void main(String[] args) {
+        String tamanho = "P";
+
+        switch (tamanho){
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MÉDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+            default: 
+                System.out.println("INDEFINIDO");
+                
+        }
+    }
+}
