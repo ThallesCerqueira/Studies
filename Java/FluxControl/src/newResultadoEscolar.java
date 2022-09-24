674ae6a72fe7ce0031254facf53d9fe9ec8430b6
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-09-24 12:24:02 -0300

    add newResultadoEscolar

---
+++ Java/FluxControl/src/newResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class newResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+        
+        if(nota >= 7){
+            System.out.println("Aprovado!");
+        }else if(nota >= 5 && nota < 7){
+            System.out.println("Aluno em recuperação!");
+        }else{
+            System.out.println("Aluno reprovado!");
+        }
+    }
+}
