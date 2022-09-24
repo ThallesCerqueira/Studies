0ec8a2864eb67c17f91af5b9d35caf14c8b73adb
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-09-24 11:13:37 -0300

    add App and CaixaEletronico

--- Java/methods/src/sobrecarga/sobrecarga.java
+++ Java/methods/src/sobrecarga/sobrecarga.java
@@ -1,2 +1,25 @@
-package sobrecarga;public class sobrecarga {
+package sobrecarga;
+
+public class sobrecarga {
+    public static void area(int lado){
+        int ladoQuadrado = lado * lado;
+        System.out.println("A área do quadrado é: " + ladoQuadrado);
+    }
+
+    public static void area(int lado1, int lado2) {
+        int areaQuadrado = lado1 * lado2;
+        System.out.println("A área do quadrado é: " + areaQuadrado);
+
+    }
+
+    public static void area(int baseMaior, int baseMenor, int altura){
+        float areaTrapezio = ((baseMaior + baseMaior) * altura) / 2;
+        System.out.println("A área do trapézio é: " + areaTrapezio);
+    }
+
+    public static void area(int dMaior, float dMenor){
+        float areaLosango = dMaior*dMenor/2;
+        System.out.println("A área do losango é: " + areaLosango);
+
+    }
 }
