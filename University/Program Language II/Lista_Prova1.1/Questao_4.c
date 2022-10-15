75189a2e15de06fe444bd2a11e8de7ce00048c9a
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-10-15 15:36:12 -0300

    Consegui localizar a letra.

--- University/Program Language II/Lista_Prova1.1/Questao_4.c
+++ University/Program Language II/Lista_Prova1.1/Questao_4.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 
+int funcao(char *string, char letra, int *posicoes, int *tamanho);
+
 int main(){
 
-    
+    //Declaração e inicializacao das variaveis
+    int *posicoes;
 
+    //posicoes = (int*) malloc(sizeof(int));
+    char *string = "agor";
+    char letra = 'a';
+    int *tamanho;
+
+    //chamada da função 
+    funcao(string, letra, posicoes, tamanho);
+
+    //TESTE PARA VETOR DE POSICOES
+    printf("VALOR: %i\n", posicoes[0]);
 
     return 0;
+}
+
+int funcao(char *string, char letra, int *posicoes, int *tamanho){
+    
+    int contador = 0;
+
+    while(*string != '\0'){
+        //Verificando a letra selecionada
+        if(*string == letra){
+            contador++;
+            *posicoes = contador;
+        }
+        string++;
+    }
+
+
 }
