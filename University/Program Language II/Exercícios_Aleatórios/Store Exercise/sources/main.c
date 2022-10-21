1419f832ede59331a4bad0b4da461b207e3fb13a
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-10-21 10:18:29 -0300

    Editing

--- University/Program Language II/Exercícios_Aleatórios/Store Exercise/sources/main.c
+++ University/Program Language II/Exercícios_Aleatórios/Store Exercise/sources/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 //Menus
-void menu_Principal();
+void menu_Principal(); //Funcionando
+void menu_cadastro_cliente();
 
 //basics
-int opcao_Menu();
+int opcao_Menu(); //Funcionando
+void cadastro_cliente();
 
 int main(){
 
@@ -12,18 +14,23 @@ int main(){
     int opcao;
 
     while(1){
+        //Chamada de funcoes
         menu_Principal();
         opcao = opcao_Menu();
+
+        //Decidindo chamada de funcoes
+        switch(opcao){
+            case 1:
+                cadastro_cliente();
+        }
         
     }
-
-
-
 
     return 0;
 }
 
 void menu_Principal(){
+    printf("MENU PRINCIPAL: \n");
     printf("\n1 - Cadastro de clientes\n");
     printf("2 - Cadastro de produtos\n");
     printf("3 - Venda\n");
@@ -45,4 +52,19 @@ int opcao_Menu(){
     }while(opcao < 1 || opcao > 4);
 
     return opcao;
+}
+
+void cadastro_cliente(){
+    menu_cadastro_cliente();
+    opcao_Menu();
+}
+
+void menu_cadastro_cliente(){
+
+    printf("\nMENU - CADASTRO CLIENTE: \n");
+    printf("\n1 - Incluir cliente\n");
+    printf("2 - Excluir cliente\n");
+    printf("3 - Alterar cliente\n");
+    printf("4 - Retornar");
+
 }
