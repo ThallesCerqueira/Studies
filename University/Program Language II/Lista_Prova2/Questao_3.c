4d9aebfd37e0d8c1c37b0ea3d79d00ef8dfc46ec
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-11-15 15:22:18 -0300

    Conclusão da questão 3

--- University/Program Language II/Lista_Prova2/Questao_3.c
+++ University/Program Language II/Lista_Prova2/Questao_3.c
@@ -11,17 +11,24 @@ typedef struct no No;
 //Procedimento para inserir no início
 No* inserirInicio(No* inicio, int dado);
 void imprimir(No* inicio);
+No* removeNo(No* inicio, int posNo);
 
 int main(){
 
     No* inicio;
 
     inicio = NULL;
+    inicio = inserirInicio(inicio, 5);
+    inicio = inserirInicio(inicio, 4);
+    inicio = inserirInicio(inicio, 3);
     inicio = inserirInicio(inicio, 2);
     inicio = inserirInicio(inicio, 1);
 
+    inicio = removeNo(inicio, 4);
+
+    printf("\n");
+
     imprimir(inicio);
-    
 
     return 0;
 }
@@ -46,6 +53,34 @@ void imprimir(No* inicio){
     for(p = inicio; p != NULL; p = p->proximo){
         printf("%dº nó\n", p->dado);
     }
+}
 
+No* removeNo(No* inicio, int posNo){
+    No *auxiliar, *ponteiroParaSeguinte;
+    
+    if(posNo == 1){
+        inicio = inicio->proximo;
+    }else{
+        
+        auxiliar = inicio;
+        for(int posicao = 1; posicao <= posNo; posicao++){
+            auxiliar = auxiliar->proximo;
+            if(posicao == (posNo - 1)){
+                ponteiroParaSeguinte = auxiliar->proximo;
+            }
+        }
 
+        auxiliar = inicio;
+        for(int posicao = 1; posicao < posNo; posicao++){
+               
+            if(posicao == (posNo - 1)){
+                auxiliar->proximo = ponteiroParaSeguinte;
+            }else{
+                auxiliar = auxiliar->proximo;
+            }
+        }
+        
+    }
+
+    return inicio;
 }
