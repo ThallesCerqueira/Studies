f252ba6a5c32c0b40a989848ea40fe25fb354a96
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-11-21 11:10:28 -0300

    Melhorando questao3

--- University/Program Language II/Lista_Prova1/questao3.c
+++ University/Program Language II/Lista_Prova1/questao3.c
@@ -3,8 +3,8 @@
 
 int* uniao(int *A, int n, int *B, int m, int *C);
 void preencher_vetor(int *A, int n, int *B, int m);
-int verifica_duplicidade(int *C, int tamanho);
-int remove_valor(int *C, int tamanho, int posicao);
+int* verifica_duplicidade(int *C, int tamanho);
+int* remove_valor(int *C, int tamanho, int posicao);
 
 int tamanhoC = 0;
 
@@ -12,7 +12,7 @@ int main(){
 
     int *A; 
     int *B; 
-    int *C, n, m, tamanho;
+    int *C, n, m;
 
     n = 3;
     m = 3;
@@ -22,7 +22,14 @@ int main(){
 
     preencher_vetor(A, n, B, m);
     C = uniao(A, n, B, m, C);
-    tamanhoC = verifica_duplicidade(C, tamanhoC);
+
+    C = verifica_duplicidade(C, tamanhoC);
+
+    printf("Novo vetor, sem duplicidades: \n");
+    for(int i = 0; i < tamanhoC; i++){
+        printf("C[%d]: %d\n", i, C[i]);
+    }
+
     printf("Tamanho: %d\n", tamanhoC);
 
     return 0;
@@ -46,7 +53,7 @@ int* uniao(int *A, int n, int *B, int m, int *C){
     
     tamanhoC = n + m;
 
-    C = (int*) malloc((n+m) * sizeof(int));
+    C = (int*) malloc(tamanhoC * sizeof(int));
 
     for(int i = 0; i < n; i++){
         C[i] = A[i];
@@ -55,32 +62,26 @@ int* uniao(int *A, int n, int *B, int m, int *C){
     for(int i = 0;i < m; i++){
         C[i + n] = B[i];
     }
-
-    for(int i = 0; i < (n+m); i++){
-        printf("C[%d]: %d\n", i, C[i]);
-    }
     
     return C;
 }
 
-int verifica_duplicidade(int *C, int tamanho){
-    //variavel auxiliar
+int* verifica_duplicidade(int *C, int tamanho){
     int aux = C[0];
-    
     //verificando igualdade
     for(int i = 0; i < tamanho; i++){
         for(int j = (i+1); j < tamanho; j++){
             if(C[i] == C[j]){
-                //chamada da funcao remove_valor
-                tamanho = remove_valor(C, tamanho, j);
+                C = remove_valor(C, tamanho, j);
+                tamanhoC--;
             }
         }
     }
 
-    return tamanho;
+    return C;
 }
 
-int remove_valor(int *C, int tamanho, int posicao){
+int* remove_valor(int *C, int tamanho, int posicao){
     
     //transposicao dos valores
     for(int i = posicao; i < tamanho; i++){
@@ -88,6 +89,6 @@ int remove_valor(int *C, int tamanho, int posicao){
     }
 
     //realocacao de memoria para Vetor
-    C = (int*) realloc(C, tamanho-1);
-    return (tamanho - 1);
+    C = (int*) realloc(C, tamanho-1); 
+    return C;
 }
