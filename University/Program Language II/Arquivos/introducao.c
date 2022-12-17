1274ffd140318262ef44dce0e53487148b555b5b
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2022-12-17 16:56:31 -0300

    escrever e ler dados de Arquivos.

--- University/Program Language II/Arquivos/introducao.c
+++ University/Program Language II/Arquivos/introducao.c
@@ -4,15 +4,9 @@ int main(){
 
     //Declaracao de ponteiro para arquivo
     FILE *fp;
-
     //vinculando arquivo ao ponteiro
-    fp = fopen("introducao.txt", "w");
-
-
+    fp = fopen("C:\\Users\\thall\\Thalles\\University\\Program Language II\\Arquivos\\introducao.txt", "w");
     fprintf(fp, "Testando a função fprintf.");
-
     //fechamento do arquivo
     fclose(fp);
-
-
 }
