52acec72baace808b0cb0dcea7c04d2fc3fed220
Thalles Cerqueira <thalles.cerqueira@cepedi.org.br>
2023-05-19 19:38:10 -0300

    new files

---
+++ University/Program Language III/Exercuse List VII/src/Main.java
@@ -0,0 +1,15 @@
+import java.time.Instant;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Date data = new Date();
+
+        System.out.println(data.getHours());
+        System.out.println(data.getMinutes());
+        System.out.println(data.getSeconds());
+        
+    }
+}
