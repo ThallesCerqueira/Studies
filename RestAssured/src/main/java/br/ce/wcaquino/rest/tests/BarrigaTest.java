fcf114da30c6547751e5877a917aade392a8115d
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-06-02 14:40:12 -0300

    Estudo do RestAssured

--- RestAssured/src/main/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
+++ RestAssured/src/main/java/br/ce/wcaquino/rest/tests/BarrigaTest.java
@@ -73,4 +73,70 @@ public class BarrigaTest extends BaseTest {
 
     }
 
+    @Test
+    public void deveAlterarInserirContaComMesmoNome() {
+
+        given()
+                .header( "Authorization", "JWT " + TOKEN )
+                .body( "{ \"nome\": \"ContaAlterada\" }" )
+        .when()
+                .post( "/contas" )
+        .then()
+                .statusCode( 400 )
+                .body( "error", is( "Já existe uma conta com esse nome!" ))
+        ;
+
+    }
+
+    @Test
+    public void deveInserirUmaMovimentacaoComSucesso() {
+
+        Movimentacao mov = new Movimentacao();
+
+        mov.setConta_id( 1740434 );
+        //mov.setUsuario_id(  );
+        mov.setDescricao( "Descrição da movimentação." );
+        mov.setEnvolvido( "Envolvido na mov" );
+        mov.setTipo( "REC" );
+        mov.setData_transacao( "01/01/2000" );
+        mov.setData_pagamento( "01/01/2010" );
+        mov.setValor( 100f );
+        mov.setStatus( true );
+
+        given()
+                .header( "Authorization", "JWT " + TOKEN )
+                .body( mov )
+        .when()
+                .post( "/transacoes" )
+        .then()
+                .statusCode( 201 )
+
+        ;
+
+    }
+
+    @Test
+    public void deveValidarCamposObrigatoriosNaMovimentacao() {
+
+        given()
+                .header( "Authorization", "JWT " + TOKEN )
+                .body( "{}" )
+        .when()
+                .post( "/transacoes" )
+        .then()
+                .statusCode( 400 )
+                .body( "$", hasSize( 8 ) )
+                .body( "msg", hasItems( "Data da Movimentação é obrigatório",
+                        "Data do pagamento é obrigatório",
+                        "Descrição é obrigatório",
+                        "Interessado é obrigatório",
+                        "Valor é obrigatório",
+                        "Valor deve ser um número",
+                        "Conta é obrigatório",
+                        "Situação é obrigatório") )
+
+        ;
+
+    }
+
 }
