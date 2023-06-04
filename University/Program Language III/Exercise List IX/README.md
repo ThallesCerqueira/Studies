abbba9c9345342f614d6113955a5324e7e44e42e
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-06-04 12:02:12 -0300

    Estudando

--- University/Program Language III/Exercise List IX/README.md
+++ University/Program Language III/Exercise List IX/README.md
@@ -1,5 +1,8 @@
 ### Hi! This exercise list is for practice concepts about heritage and Method Override.
 
 ### List of questions:
-    1 - OK
-    2 - Metade
+    1 - OK.
+    2 - Falta b e d.
+    3 - OK.
+    4 - 
+    5 - 
---
+++ University/Program Language III/Exercise List IX/src/LivroBiblioteca.java
@@ -0,0 +1,39 @@
+public class LivroBiblioteca extends Livro {
+
+    private boolean emprestado;
+    private String dataDevolucao;
+
+    public LivroBiblioteca( String titulo, String autor, int nroPaginas, int anoPublicacao ) {
+        super( titulo, autor, nroPaginas, anoPublicacao );
+        this.emprestado = false;
+        this.dataDevolucao = "";
+    }
+
+    public LivroBiblioteca() {
+        this( "", "", 1, 1900 );
+    }
+
+    public String toString() {
+        return "Título: " + super.getTitulo() + "\nAutor: " + super.getAutor() + "\nNúmero Páginas: " +
+                super.getNroPaginas() + "\nAno Publicação: " + super.getAnoPublicacao() + "\nEmprestado: " + this.emprestado
+                + "\nData Devolução: " + this.dataDevolucao;
+    }
+
+    public boolean isOlder(LivroBiblioteca livro) {
+        return super.getAnoPublicacao() > livro.getAnoPublicacao();
+    }
+
+    public boolean isAvailable() {
+        return !this.emprestado;
+    }
+
+    public boolean borrow() {
+
+        if( isAvailable() ) {
+            this.emprestado = true;
+            return true;
+        }
+
+        return false;
+    }
+}
