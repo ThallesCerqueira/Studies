ba009f4fcf1d58d8201807b1b5bed6d109ef37c1
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-07-03 20:52:03 -0300

    adicao de atributos estaticos

--- University/Program Language III/Projeto - Sigma Bank/src/produtosBancarios/financiamentos/FinanciamentoVeicular.java
+++ University/Program Language III/Projeto - Sigma Bank/src/produtosBancarios/financiamentos/FinanciamentoVeicular.java
@@ -2,12 +2,14 @@ package produtosBancarios.financiamentos;
 
 public class FinanciamentoVeicular extends Financiamento {
 
+    private static final double taxa = 0.20d;
+
     // Construtor com 2 parâmetros
     public FinanciamentoVeicular( int parcelas, double valor ) {
 
         // Chamando Construtor da Super Class
         // Diferença na taxa de juros
-        super( parcelas, valor + (valor * 0.20d) );
+        super( parcelas, valor + (valor * taxa) );
     }
 
 }
