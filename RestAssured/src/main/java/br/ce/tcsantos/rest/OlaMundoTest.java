62df7df0c6b52726705a604a373f2001a6fd46be
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-09-29 08:47:37 -0300

    estudando Restassured

--- RestAssured/src/main/java/br/ce/tcsantos/rest/OlaMundoTest.java
+++ RestAssured/src/main/java/br/ce/tcsantos/rest/OlaMundoTest.java
@@ -4,7 +4,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
 import org.junit.Test;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 
 public class OlaMundoTest {
 
@@ -12,9 +15,30 @@ public class OlaMundoTest {
     public void testOlaMundo() {
 
         Response response = RestAssured.request( Method.GET, "https://restapi.wcaquino.me/ola" );
+        Assert.assertEquals("Ola Mundo!", response.getBody().asString());
+        Assert.assertEquals(200, response.statusCode());
 
         ValidatableResponse validacao = response.then();
         validacao.statusCode(200);
+
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured() {
+
+        Response response = RestAssured.request( Method.GET, "https://restapi.wcaquino.me/ola" );
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+
+        get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/ola")
+        .then()
+                .statusCode(200)
+        ;
 
     }
 
