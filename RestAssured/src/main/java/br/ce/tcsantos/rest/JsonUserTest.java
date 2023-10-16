fc9a058d4156d11fb91ae26ace059bb513cb1e55
ThallesCerqueira <thallescerqueira.dev@gmail.com>
2023-10-16 11:32:53 -0300

    Aprendendo a fazer vários tipos de validação, para respostas Json

--- RestAssured/src/main/java/br/ce/tcsantos/rest/JsonUserTest.java
+++ RestAssured/src/main/java/br/ce/tcsantos/rest/JsonUserTest.java
@@ -6,6 +6,10 @@ import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
@@ -77,4 +81,76 @@ public class JsonUserTest {
 
         ;
     }
+
+    // AULA 14
+    @Test
+    public void deveRetornarErroUsuarioInexistente() {
+
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/users/4")
+        .then()
+                .statusCode(404)
+                .body("error", is("Usuário inexistente"))
+        ;
+    }
+
+    // AULA 15
+    @Test
+    public void deveVerificarListaNaRaiz() {
+
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/users")
+        .then()
+                .statusCode(200)
+                .body("$", hasSize(3)) // Valida tamanho do array raiz
+                .body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia")) // Verifica nas chaves name os nomes listados
+                .body("age[1]", is(25)) // Verifica a idade da chave age para o elemento de posicao 1 no array raiz
+                .body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho"))) // Verifica se tem os nomes listados nas chave filhos.name do array raiz
+                .body("salary", contains(1234.5678f, 2500, null)) // Verifica os salarios listados, para a chave salary do array raiz
+         ;
+
+    }
+
+    // AULA 16
+    @Test
+    public void verificacoesAvancadas() {
+
+        given()
+        .when()
+                .get("https://restapi.wcaquino.me/users")
+        .then()
+                .statusCode(200)
+                .body("$", hasSize(3)) // Valida tamanho do array raiz
+                .body("age.findAll{it <= 25}.size()", is(2)) // Validando qtd de users com idade <= 25
+                .body("age.findAll{it > 20 && it <= 25}.size()", is(1)) // Validando qtd de users com idade > 20 e idade <= 25
+                .body("findAll{it.age > 20 && it.age <= 25}.name", hasItem("Maria Joaquina")) // Verificando existencia do item x nos usarios que tem idade restrita
+                .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia")) // Validando nomes com letra 'n'
+                .body("findAll{it.name.length() > 10}.name", hasItems( "João da Silva","Maria Joaquina")) // Validando nomes com tamanho > 10
+        ;
+
+    }
+
+    // AULA 17
+    @Test
+    public void unirJsonPathComJava() {
+
+        // Fazendo requisição e extraindo os elementos que começam com Maria
+        ArrayList<String> names = given()
+                                .when()
+                                .get("https://restapi.wcaquino.me/users")
+                                .then()
+                                        .statusCode(200)
+                                        .extract().path("name.findAll{it.startsWith('Maria')}")
+                                ;
+
+        // Validando tamanho do arralist names
+        Assert.assertEquals(1, names.size());
+
+        // Validando o nome da primeira posição, ignorando case sensitive.
+        Assert.assertTrue(names.get(0).equalsIgnoreCase("Maria Joaquina"));
+
+    }
+
 }
